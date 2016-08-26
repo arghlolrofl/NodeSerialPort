1f21b2003b610cfea5e05bac94e7cf5a0c713d81
Kai Nicolai <kai_nicolai@hotmail.com>
2016-08-26 14:11:06 +0200

    initial revision

---
+++ labor/nicolai/TestSuite/TestSuite/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Devices.SerialCommunication;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TestSuite
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var deviceSelector = SerialDevice.GetDeviceSelector();
+            var devices = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(deviceSelector);
+            StringBuilder sb = new StringBuilder();
+            if (devices.Any() == false)
+            {
+                m_Textbox.Text = "No devices found";
+            }
+            else
+            {
+                foreach (var device in devices)
+                {
+                    sb.AppendLine($"Name: {device.Name} ID: {device.Id} ");
+                }
+                m_Textbox.Text = sb.ToString();
+            }
+        }
+    }
+}
