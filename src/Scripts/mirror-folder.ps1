f90368c38d214b22453a91e104d0968f34ee47ae
Irmischer <h.irmischer@francotyp.com>
2016-09-01 13:19:48 +0200

    1. Added deployment task for command task runner extension of VS2015, which executes a powershell script on post build event which then copies all project files to the device via robocopy. (See colution folder\doc\Windows IoT - Dev Environment Setup.docx for detailed information)

---
+++ src/Scripts/mirror-folder.ps1
@@ -0,0 +1,9 @@
+﻿param([string]$Source, [string]$Dest)
+
+# Either use folders as parameter or define them here
+# Source Folder
+#$pathDeploySource = "D:\projects\NavigatorWeb\src\NavigatorWeb.Node\bin\ARM\Debug\AppX\"
+# Destination Folder
+#$pathDeployDest = "W:\"
+
+robocopy "$($Source)" "$($Dest)" /MIR /NC /NP /NS /NDL /ETA /XF "vs.appxrecipe" ".gitignore"
